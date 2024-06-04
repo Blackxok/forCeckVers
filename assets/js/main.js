9f3d97859348b249c90a683edaa41a01504190ae
Blackxok <nasridoni1@gmail.com>
2024-06-04 11:20:30 +0500

    cng VA makanesta

--- assets/js/main.js
+++ assets/js/main.js
@@ -9,4 +9,24 @@ document.addEventListener('DOMContentLoaded', function () {
 			this.classList.add('active')
 		})
 	})
+	// outside effect
+	const color = 'rgba(0, 26, 255, 1)' /* white outline */
+	const r = 10 /* width of outline in pixels */
+	const n = Math.ceil(2 * Math.PI * r) /* number of shadows */
+	var str = ''
+	for (
+		var i = 0;
+		i < n;
+		i++ /* append shadows in n evenly distributed directions */
+	) {
+		const theta = (2 * Math.PI * i) / n
+		str +=
+			r * Math.cos(theta) +
+			'px ' +
+			r * Math.sin(theta) +
+			'px 0 ' +
+			color +
+			(i == n - 1 ? '' : ',')
+	}
+	document.querySelector('.va').style.textShadow = str
 })
