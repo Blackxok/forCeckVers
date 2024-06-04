5daded4ebd704c4770f47d1d658d0bfa7ef558ad
Blackxok <nasridoni1@gmail.com>
2024-06-04 21:14:55 +0500

    cng last time

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', function () {
+	//////////////////////////////////// nav
 	var buttons = document.querySelectorAll('.nav_btn')
-
 	buttons.forEach(function (button) {
 		button.addEventListener('click', function () {
 			buttons.forEach(function (btn) {
@@ -9,16 +9,12 @@ document.addEventListener('DOMContentLoaded', function () {
 			this.classList.add('active')
 		})
 	})
-	// outside effect
-	const color = 'rgba(0, 26, 255, 1)' /* white outline */
-	const r = 10 /* width of outline in pixels */
-	const n = Math.ceil(2 * Math.PI * r) /* number of shadows */
+	//////////////////////////////////// outside effect
+	const color = 'rgba(0, 26, 255, 1)'
+	const r = 10
+	const n = Math.ceil(2 * Math.PI * r)
 	var str = ''
-	for (
-		var i = 0;
-		i < n;
-		i++ /* append shadows in n evenly distributed directions */
-	) {
+	for (var i = 0; i < n; i++) {
 		const theta = (2 * Math.PI * i) / n
 		str +=
 			r * Math.cos(theta) +
@@ -29,4 +25,21 @@ document.addEventListener('DOMContentLoaded', function () {
 			(i == n - 1 ? '' : ',')
 	}
 	document.querySelector('.va').style.textShadow = str
+	//////////////////////////////////// outside effect
+})
+const h3 = document.querySelectorAll('.r_side_item_title')
+
+h3.forEach(h3_item => {
+	h3_item.addEventListener('click', () => {
+		const p = h3_item.nextElementSibling // Get the next paragraph element
+		p.style.display = p.style.display
+		if (p.style.display === 'none') {
+			p.style.display = 'block'
+			console.log(h3_item)
+			h3_item::after
+		} else {
+			p.style.display = 'none'
+			// h3_item.classList.remove('.active')
+		}
+	})
 })
